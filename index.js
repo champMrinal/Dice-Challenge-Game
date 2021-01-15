9ebe6cc4db746009ca216644903db331232fadbc
champMrinal <76103475+champMrinal@users.noreply.github.com>
2021-01-15 21:57:40 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// for image 1
+var randomNumber1= Math.floor(Math.random() * 6)+1;
+
+var randomDiceImage = "dice"+randomNumber1+".png";
+var randomImageSourse="images/"+randomDiceImage;
+var image1= document.querySelectorAll("img")[0];
+image1.setAttribute("src",randomImageSourse);
+
+
+// for image 2
+var randomNumber2=Math.floor(Math.random()* 6)+1;
+var randomDiceImage2 ="dice"+randomNumber2+".png";
+var randomImageSourse2="images/"+randomDiceImage2;
+var image2=document.querySelectorAll("img")[1];
+image2.setAttribute("src",randomImageSourse2);
+
+if(randomNumber1>randomNumber2)
+{
+    document.querySelector("h1").innerHTML="Player1 wins!"
+}
+else if(randomNumber2>randomNumber1)
+{
+    document.querySelector("h1").innerHTML="Player2 wins!"
+
+}
+else
+{
+    document.querySelector("h1").innerHTML="Draw!!!!"
+}
